4ee4306058f192a5778cba80a02df69e437673a8
Hakim Benoudjit <h.benoudjit@gmail.com>
2019-08-04 20:01:43 +0100

    Todo list with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
+// Simple todo list
+// https://medium.com/@aghh1504/1-simple-react-todo-list-52186b62976b
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import List from './List.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            item: '',
+            items: []
+        };
+
+        // binding necessary to make 'this' work in callback
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        // save entered value
+        this.setState({item: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        // append entered value & clear input
+        if (this.state.item)
+            this.setState({items: [...this.state.items, this.state.item], item: ''}, () => {  
+                console.log('Callback');
+                console.log(this.state.items);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" onChange={this.onChange} value={this.state.item} />
+                    <button className={styles.button}>Submit</button>
+                </form>
+
+                <List items={this.state.items} />
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List(props) {
+    return (
+        <ul>
+            {props.items.map((item, key) => <li key={key}>{item}</li>)}
+        </ul>
+    );
+}
+
+export default List;
